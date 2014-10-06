b984c93928218692306dad15b08d88d04d5ce13e
John Cowie <john.a.cowie@gmail.com>
2014-10-06 09:40:06 +0100

    Added components for doing HTTP gets, and stubbing them

--- src/dunbar/http.clj
+++ src/dunbar/http.clj
@@ -1,2 +1,26 @@
 (ns dunbar.http
   (:require [clj-http.client :as http]))
+
+(defprotocol HTTP
+  (http-get [this uri])
+  )
+
+(defrecord TestHTTP [next-responses]
+  HTTP
+  (http-get [this uri]
+    (if (empty? @next-responses)
+      (throw (Exception. "no more responses defined"))
+      (let [r (first @next-responses)]
+        (swap! next-responses rest)
+        r))))
+
+(defrecord RealHTTP []
+  HTTP
+  (http-get [this uri]
+    (http/get uri)))
+
+(defn new-test-http [next-responses]
+  (TestHTTP. (atom next-responses)))
+
+(defn new-http []
+  (RealHTTP.))
