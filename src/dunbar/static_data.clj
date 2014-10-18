c62a12be82223272151da23c86fa5325c56cdaba
John Cowie <john.a.cowie@gmail.com>
2014-10-18 18:15:16 +0100

    Added more options for meet frequency

--- src/dunbar/static_data.clj
+++ src/dunbar/static_data.clj
@@ -3,5 +3,8 @@
 (def meet-freq
   {1 "once a day"
    7 "once a week"
+   14 "once every two weeks"
    28 "once a month"
+   90 "once every three months"
+   180 "once every six months"
    365 "once a year"})
